8003016de970028d7841cddf7a6a049beb915d72
oluwatoyin oredein <teboola2@yahoo.com>
2023-07-24 05:13:45 +0100

    edit lorem text

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ const nav = document.querySelector('.nav');
 navToggle.addEventListener('click', () => {
     nav.classList.toggle('nav--visible');
 })
+const day = new Date();
+const year = day.getFullYear();
+let cpYear = document.getElementById('year');
+cpYear.innerHTML = year;
